857bc6059b3a280f453a1260eef72caa94daa495
knightofl <drhoney@gmail.com>
2025-01-11 21:48:17 +0900

    lisp

--- lisp/basic_test/test14.lisp
+++ lisp/basic_test/test14.lisp
@@ -6,7 +6,7 @@
 (defun add-two-numbers ()
     (let ((num1 (read-number))
           (num2 (read-number)))
-        (princ (+ num1 num2))))
+        (format t "~D + ~D = ~D~%" num1 num2 (+ num1 num2))))
 
 (add-two-numbers)
          
