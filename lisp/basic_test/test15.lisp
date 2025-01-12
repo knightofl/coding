e611d446e44fea8758aea1e380731f528e503e93
knightofl <drhoney@gmail.com>
2025-01-12 11:00:59 +0900

    lisp

--- lisp/basic_test/test15.lisp
+++ lisp/basic_test/test15.lisp
@@ -10,4 +10,12 @@
         (format t "The sum is ~D~%" sum)
         sum))
 
-(add-numbers 5)
+;(add-numbers 5)
+
+(defun add-numbers-2 (n)
+    (let ((sum  0))
+        (do ((i 0 (1+ i)))
+            ((= i n) (format t "The sum is ~D~%" sum) sum)
+            (setf sum (+ sum (read-number))))))
+
+(add-numbers-2 5)
