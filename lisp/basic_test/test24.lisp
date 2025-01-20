7adf6f31d8a83d7fecf00d53169ff1f05ee98f8f
knightofl <drhoney@gmail.com>
2025-01-20 20:24:46 +0900

    lisp

--- lisp/basic_test/test24.lisp
+++ lisp/basic_test/test24.lisp
@@ -4,3 +4,9 @@
 (my-when t (lambda () (princ "hello")
                       (princ "world")))
 
+(defmacro my-and (&rest exprs)
+    (if (= (length exprs) 1)
+        (first exprs)
+        `(if ,(first exprs)
+             ,(first exprs)
+        (my-and ,@(rest exprs)))))
---
+++ lisp/basic_test/test25.lisp
@@ -0,0 +1,12 @@
+;; Usage Example
+(if-let (x 12)
+    (princ x) (princ "got nil"))
+
+;; Implementation
+(defmacro if-let ((x x-val) true-expr false-expr)
+    `(let ((,x ,x-val))
+        (if ,x ,true-expr ,false-expr)))
+
+(defmacro if-let (binding true-expr false-expr)
+    `(let (,binding)
+        (if ,(car binding) ,true-expr ,false-expr)))
