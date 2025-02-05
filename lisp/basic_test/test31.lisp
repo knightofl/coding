f72854d4c0f4162519b25b0dde14b87cd647171c
knightofl <drhoney@gmail.com>
2025-02-05 11:12:18 +0900

    lisp

--- lisp/basic_test/test31.lisp
+++ lisp/basic_test/test31.lisp
@@ -1,7 +1,19 @@
-(defparammeter *my-circle*
-    (make-instance 'circle :radius 10))
-(princ (area *my-circle*))
+(defun save-pattern (filename grid)
+  (with-open-file (out filename :direction :output :if-exists :supersede)
+    (dolist (row grid)
+      (dolist (cell row)
+        (write-char (if cell #\# #\.) out))
+    (terpri out))))
 
-(defparameter *my-rect* 
-    (make-instance 'rectangle :base 10 :height 12))
-(princ (area *my-rect*))
+(defun load-pattern (filename)
+  (if (probe-file filename)
+      (with-open-file (in filename :direction :input)
+        (loop for line = (read-line in nil)
+              while line
+              collecting (map 'vector (lambda (ch) (if (char= ch #\#) 1 0) line)) into grid
+              finally (return (coerce grid 'vector))))
+        (error "File not found: ~A" filename)))
+
+(defun start-new-game (fliename)
+  (setf *grid* (load-pattern filename))
+  (run-life))
