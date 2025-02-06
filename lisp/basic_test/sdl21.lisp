0df875e42c3fcbbfca23e5b22f7a0b726938053e
knightofl <drhoney@gmail.com>
2025-02-07 07:52:42 +0900

    lisp

--- lisp/basic_test/sdl21.lisp
+++ lisp/basic_test/sdl21.lisp
@@ -1,48 +1,60 @@
 (load "~/quicklisp/setup.lisp")
 (ql:quickload "sdl2")
 
-(defparameter *width* 800)
+;;; Configuration
+(defparameter *width*  800)
 (defparameter *height* 600)
-(defparameter *max-depth* 7)  ; 재귀 깊이
+(defparameter *max-depth* 7)
+(defparameter *background-color* '(0 0 0 255))
+(defparameter *triangle-color*   '(0 255 0 255))
 
-(defun draw-triangle (renderer x1 y1 x2 y2 x3 y3 depth)
-  (if (<= depth 0)
-      (progn
-        (sdl2:render-draw-line renderer x1 y1 x2 y2)
-        (sdl2:render-draw-line renderer x2 y2 x3 y3)
-        (sdl2:render-draw-line renderer x3 y3 x1 y1))
-      (let* ((x12 (/ (+ x1 x2) 2))
-             (y12 (/ (+ y1 y2) 2))
-             (x23 (/ (+ x2 x3) 2))
-             (y23 (/ (+ y2 y3) 2))
-             (x31 (/ (+ x3 x1) 2))
-             (y31 (/ (+ y3 y1) 2)))
-        (draw-triangle renderer x1 y1 x12 y12 x31 y31 (1- depth))
-        (draw-triangle renderer x12 y12 x2 y2 x23 y23 (1- depth))
-        (draw-triangle renderer x31 y31 x23 y23 x3 y3 (1- depth)))))
+;;; Math Utilities
+(defun mid (a b) (floor (+ a b) 2))
 
+;;; Drawing Core
+(defun draw-triangle (renderer p1 p2 p3 depth)
+  (destructuring-bind ((x1 y1) (x2 y2) (x3 y3)) (list p1 p2 p3)
+    (if (zerop depth)
+        ;; Base case: Draw the triangle
+        (progn
+          (sdl2:render-draw-line renderer x1 y1 x2 y2)
+          (sdl2:render-draw-line renderer x2 y2 x3 y3)
+          (sdl2:render-draw-line renderer x3 y3 x1 y1))
+        ;; Recursive case: Split and recurse
+        (let ((m12 (list (mid x1 x2) (mid y1 y2)))
+              (m23 (list (mid x2 x3) (mid y2 y3)))
+              (m31 (list (mid x3 x1) (mid y3 y1))))
+          (draw-triangle renderer p1 m12 m31 (1- depth))
+          (draw-triangle renderer m12 p2 m23 (1- depth))
+          (draw-triangle renderer m31 m23 p3 (1- depth))))))
+
+;;; Main Drawing Function
 (defun draw-sierpinski (renderer)
-  (sdl2:set-render-draw-color renderer 255 255 255 255)  ; 배경색: 흰색
-  (sdl2:render-clear renderer)
-  (sdl2:set-render-draw-color renderer 0 0 0 255)  ; 삼각형 색상: 검은색
-  (let ((x1 (/ *width* 2))
-        (y1 50)
-        (x2 50)
-        (y2 (- *height* 50))
-        (x3 (- *width* 50))
-        (y3 (- *height* 50)))
-    (draw-triangle renderer x1 y1 x2 y2 x3 y3 *max-depth*))
+  (destructuring-bind (r g b a) *background-color*
+    (sdl2:set-render-draw-color renderer r g b a)
+    (sdl2:render-clear renderer))
+  
+  (destructuring-bind (r g b a) *triangle-color*
+    (sdl2:set-render-draw-color renderer r g b a)
+    (let ((p1 (list (mid *width* 0) 50))          ; Top
+          (p2 (list 50 (- *height* 50)))          ; Left
+          (p3 (list (- *width* 50) (- *height* 50)))) ; Right
+      (draw-triangle renderer p1 p2 p3 *max-depth*)))
+  
   (sdl2:render-present renderer))
 
-(defun main ()
+;;; Window Management
+(defun run-sierpinski ()
   (sdl2:with-init (:video)
-    (sdl2:with-window (win :title "Sierpiński Triangle" :w *width* :h *height* :flags '(:shown))
-      (sdl2:with-renderer (renderer win :index -1 :flags '(:accelerated))
+    (sdl2:with-window (win :title "Sierpiński Masterpiece" :w *width* :h *height* :flags '(:shown))
+      (sdl2:with-renderer (renderer win)
         (draw-sierpinski renderer)
         (sdl2:with-event-loop (:method :poll)
           (:quit () t)
           (:keydown (:keysym keysym)
                     (when (sdl2:scancode= (sdl2:scancode-value keysym) :scancode-escape)
-                      (sdl2:push-quit-event))))))))
+                      (sdl2:push-quit-event)))))))
+  (sb-ext:gc :full t))
 
-(main)
+;;; Start the show!
+(run-sierpinski)
