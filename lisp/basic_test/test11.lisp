5c076d64ac612502beeec918a794ff6fdbf08f5d
knightofl <drhoney@gmail.com>
2025-02-10 11:20:43 +0900

    lisp

--- lisp/basic_test/test11.lisp
+++ lisp/basic_test/test11.lisp
@@ -6,3 +6,26 @@
             "zero")))
 
 (format t "~A~%" (sign 0))
+
+(defun print-sign-1 (n)
+    (if (< n 0)
+        (format t "negative~%")
+        (format t "positive or zeo~%")))
+
+(defun print-sign-2 (n)
+    (format t (if (< n 0)
+        "negative~%"
+        "positive or zero~%")))
+
+
+(defun print-sign-3 (n)
+    (format t "~A~%" (if (< n 0)
+        "negative"
+        "positive or zero")))
+
+(defun print-sign-4 (n)
+    (format t "~A~%" (if (< n 0)
+        "negative"
+        (if (> n 0)
+            "positive"
+            "zero"))))
