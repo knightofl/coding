994140915826b282dd3eb63dd0520bf31c8c1737
knightofl <drhoney@gmail.com>
2025-02-12 16:48:17 +0900

    lisp

--- lisp/basic_test/test28.lisp
+++ lisp/basic_test/test28.lisp
@@ -1,14 +1,14 @@
 (defmacro for ((var from to) &rest body)
-    (let ((sym-to (gensym)))
-        `(do ((,var ,from (1+ ,var))
-              (,sym-to ,to))
-             ((> ,var ,sym-to))
-             ,@body)))
-
-(defmacro for2 ((var from to) &rest body)
     `(do ((,var ,from (1+ ,var))
           (limit ,to))
          ((>= ,var limit))
          ,@body))
+
+(defmacro for ((var from to) &rest body)
+    (let ((sym-to (gensym)))
+        `(do ((,var ,from (1+ ,var))
+              (,sym-to ,to))
+             ((>= ,var ,sym-to))
+             ,@body)))
 
 (for (i 0 10) (princ i))
